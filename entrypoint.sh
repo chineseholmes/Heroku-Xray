80ac8a49e9896959c0ad29611df63d3a4de2d578
Dark Killer <86813581+ImDarkLK@users.noreply.github.com>
2022-04-06 21:04:25 +0530

    Released

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,35 @@
+#!/bin/sh
 
+#Xray-Config
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#Deploy-Xray
+
+xray -c /etc/xray/config.json
